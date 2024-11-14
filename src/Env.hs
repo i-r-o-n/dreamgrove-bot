f902a39745dbaa34cfa5a44b6c959682090f08c3
dre <a@alipson.me>
2024-11-14 00:28:41 -0800

    started to modify example code, change default logging channel to test (stored in env)

---
+++ src/Env.hs
@@ -0,0 +1,23 @@
+module Env where
+
+import qualified Data.Text     as T
+import qualified Data.Text.IO  as TIO
+import           Discord.Types
+import           Text.Read     (readMaybe)
+
+getToken :: IO T.Text
+getToken = TIO.readFile "./.env/auth-token"
+
+getGuildId :: IO GuildId
+getGuildId = do
+  gids <- readFile "./.env/guild-id"
+  case readMaybe gids of
+    Just g  -> pure g
+    Nothing -> error "could not read guild id"
+
+getTestChannelId :: IO ChannelId
+getTestChannelId = do
+  cid <- readFile "./.env/test-channel-id"
+  case readMaybe cid of
+    Just c  -> pure c
+    Nothing -> error "cound not read test channel id"
